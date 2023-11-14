560fc24bb965a0de66e93b2ba822c84814cea2c1
venkatarathnam1992 <123345822+venkatarathnam1992@users.noreply.github.com>
2023-11-14 20:11:28 +0530

    Add files via upload

---
+++ round robin non preemtive.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+struct Process {
+    int id;
+    int burst_time;
+};
+
+int main() {
+    struct Process processes[] = {
+        {6, 16},
+        {7, 5},
+        {5, 8},
+        {3, 7},
+    };
+    int num_processes = sizeof(processes) / sizeof(processes[0]);
+
+    printf("Non-Preemptive Round Robin Process execution order:\n");
+	int i;
+    for (i = 0; i < num_processes; i++) {
+        printf("Process %d (Time 0 to %d)\n", processes[i].id, processes[i].burst_time);
+    }
+
+    return 0;
+}
+
